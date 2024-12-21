ffbd2608044c9f1fe9612c87141ac3d9b97813ac
yz1311 <yz1311@foxmail.com>
2024-12-21 16:13:55 +0800

    先提交一般

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,37 @@
+import React, {FC, useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {UVCCameraWithIndex} from '@fe-free/react-native-uvc-camera';
+import {PERMISSIONS, requestMultiple} from 'react-native-permissions';
+
+const Home: FC = () => {
+  // 申请权限
+  useEffect(() => {
+    requestMultiple([
+      PERMISSIONS.ANDROID.CAMERA,
+      PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE,
+    ]);
+  }, []);
+
+  return (
+    <View>
+      <Text>相机 1</Text>
+      <View style={styles.cameraView}>
+        <UVCCameraWithIndex index={0} />
+      </View>
+      <Text>相机 2</Text>
+      <View style={styles.cameraView}>
+        <UVCCameraWithIndex index={1} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  cameraView: {
+    width: '100%',
+    height: 240,
+    backgroundColor: '#000',
+  },
+});
+
+export default Home;
